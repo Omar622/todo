5fb237ea6cf7d75f4b7b97b02882456ee390b679
Omar622 <omaramin622@gmail.com>
2023-11-05 23:54:51 +0200

    Create todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    """
+    Represents a todo item.
+
+    Fields:
+    - title (CharField): The title of the todo item (max length: 200 characters).
+    - content (TextField): The content or description of the todo item.
+    - is_completed (BooleanField): The completion status of the todo item.
+    """
+
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    is_completed = models.BooleanField()
