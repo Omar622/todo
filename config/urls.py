d8533f20115f01f1413e2fa1665bc26d13e48ed2
Omar622 <omaramin622@gmail.com>
2023-11-05 23:55:23 +0200

    Add views and urls

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework import routers
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from todo import views
+
+router = routers.DefaultRouter()
+router.register(r'todos', views.TodoViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Todo
 
-# Create your views here.
+
+class TodoViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for viewing and editing todos.
+    """
+    queryset = Todo.objects.all()
+    # serializer_class = AccountSerializer
