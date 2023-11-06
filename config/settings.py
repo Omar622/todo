36e0c7b6071f788f0b958d7d58e8c00e24b6a967
Omar622 <omaramin622@gmail.com>
2023-11-06 14:12:24 +0200

    Add pagination

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,11 @@ INSTALLED_APPS = [
     'todo'
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
